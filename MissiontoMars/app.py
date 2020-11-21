0d7abd6d76adfc93e834c999ac8a29a9ee816605
Jen-Dean <missjenniferdean@gmail.com>
2020-11-21 17:09:32 -0500

    setting up more things

---
+++ MissiontoMars/app.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template, redirect
+from flask_pymongo import PyMongo
+import scrape_costa
+
+# Create an instance of Flask
+app = Flask(__name__)
+
+# Use PyMongo to establish Mongo connection
+mongo = PyMongo(app, uri="mongodb://localhost:27017/mission_to_mars")
+
+@app.route("/")
+def home():
+
+    # Find one record of data from the mongo database
+    mars_data = mongo.db.collection.find_one()
+
+    # Return template and data
+    return render_template("index.html", vacation=mars_data)
+
+@app.route("/scrape")
+def scrape():
+
+    # Run the scrape function
+    mars_data = mars_scrape.mars_scrape()
+
+    # Update the Mongo database using update and upsert=True
+    mongo.db.collection.update({}, mars_data, upsert=True)
+
+    # Redirect back to home page
+    return redirect("/")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
